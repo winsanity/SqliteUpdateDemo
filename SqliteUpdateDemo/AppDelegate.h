e1eda6a4dc08c23eaa05e2e4bb66e57ff98f3dc1
王涛 <624782735@qq.com>
2017-09-26 14:11:12 +0800

    Initial Commit

---
+++ SqliteUpdateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SqliteUpdateDemo
+//
+//  Created by 王涛 on 2017/9/26.
+//  Copyright © 2017年 winsanity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SqliteUpdateDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SqliteUpdateDemo
+//
+//  Created by 王涛 on 2017/9/26.
+//  Copyright © 2017年 winsanity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
